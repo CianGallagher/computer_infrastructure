d084287c2f51264e66a4a142d4fa9b6d08023526
Gally <70110451+CianGallagher@users.noreply.github.com>
2024-10-13 13:03:56 +0000

    Script amended to fix the issue with the JSON file being saved into the root directory, -O and -P commands can cause conflicts

--- weather.sh
+++ weather.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
-wget -O $(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today -P /workspaces/computer_infrastructure/data/weather
+# wget -O $(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today -P /workspaces/computer_infrastructure/data/weather - Original Script with -P
+
+# Rearranged script that fixes the -O -P conflict.
+wget -O /workspaces/computer_infrastructure/data/weather/$(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today
