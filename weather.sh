0f3233f1f44fb24221df961551dc93966aa59323
CianGallagher <ciangall1@gmail.com>
2024-10-31 15:48:56 +0000

    script filepath edited and README updatedd with initial notes on the automation section of the module

--- weather.sh
+++ weather.sh
@@ -3,4 +3,4 @@
 # wget -O $(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today -P /workspaces/computer_infrastructure/data/weather - Original Script with -P
 
 # Rearranged script that fixes the -O -P conflict.
-wget -O /workspaces/computer_infrastructure/data/weather/$(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today
+wget -O /home/gally/Documents/repos/computer_infrastructure//data/weather/$(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today
