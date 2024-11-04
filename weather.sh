51286336adbb0be537566b091ba6dc186586648c
Cian Gallagher <ciangall1@gmail.com>
2024-11-04 14:24:00 +0000

    Modified weather.sh as it was only running locally, also refactored yml file, test running in 2 mins from push

--- weather.sh
+++ weather.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 
-# wget -O $(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today -P /workspaces/computer_infrastructure/data/weather - Original Script with -P
-
 # Rearranged script that fixes the -O -P conflict.
-wget -O /home/gally/Documents/repos/computer_infrastructure//data/weather/$(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today
+wget -O ./data/weather/$(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today
